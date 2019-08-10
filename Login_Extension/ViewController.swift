5bb8f41c9830934c57fddde8f38c81a6e062a1ff
LionPig <woshiwangzhezhu@gmail.com>
2019-08-10 23:43:31 +0800

    1

---
+++ Login_Extension/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login_Extension
+//
+//  Created by Mr.Zhu on 2019/8/10.
+//  Copyright © 2019 Mr.Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.0'
+
+source 'https://github.com/simplecomponent/Private_Repo.git'
+source 'https://mirrors.tuna.tsinghua.edu.cn/git/CocoaPods/Specs.git'
+
+use_frameworks!
+
+target 'Login_Extension' do
+ pod 'CTMediator'
+end
